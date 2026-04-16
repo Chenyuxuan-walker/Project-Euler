e7c0757c588cba369c2cd1efa4b047d9321814b4
Chenyuxuan-walker <yuxuanc108@gmail.com>
2026-04-16 10:45:06 +0800

    Problem 7

---
+++ Problem 7.c
@@ -0,0 +1,32 @@
+//Problem 7
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+bool is_prime (long long n) ;
+int main()
+{
+    int target_count = 10001 ;
+    int current_count = 0 ;
+    long long num = 1 ;
+    while (current_count < target_count)
+        {
+            if(is_prime(num))
+                {
+                    current_count++;
+                }
+            num++ ;
+        }
+    printf("%lld\n",num-1) ;
+}
+bool is_prime (long long n)
+{
+    if (n < 2)
+        return false ;
+    long long limit = (long long)sqrt(n) ;
+    for (long long i = 2 ; i <= limit ; i++)
+    {
+        if (n % i == 0)
+            return false ;
+    }
+    return true ;
+}
